46b6cdf5ae52905df8f0981d16ac8fd018ba56ed
Yuto Mizutani <i.yuto.curiosity@gmail.com>
2016-09-16 15:44:49 +0900

    Initial Commit

---
+++ commandLineTool/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  commandLineTool
+//
+//  Created by Yuto Mizutani on 2016/09/16.
+//  Copyright © 2016年 Yuto Mizutani. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
