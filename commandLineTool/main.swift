22e771949007cebc9cf24b9fb168deba8796fa61
Yuto Mizutani <i.yuto.curiosity@gmail.com>
2016-10-13 16:39:06 +0900

    必須イニシャライザのコード

--- commandLineTool/main.swift
+++ commandLineTool/main.swift
@@ -1,32 +1,25 @@
-
-let inputLine = readLine()!
-
-if let N = Int(inputLine){
-    print("\n" + String(N))
-    var strikeCount = 0
-    var ballCount = 0
-    
-    for i in 1 ... N - 1{
-        print(i)
-        let call = readLine()!
-        print("test:")
-        print(call)
-        
-        if call == "strike"{
-            print("strike!")
-            strikeCount += 1
-            if strikeCount == 3{
-                print("out!")
-            }
-        } else {
-            print("ball!")
-            ballCount += 1
-            if ballCount == 4{
-                print("fourball!")
-            }
-        }
-
+class ICCard {
+    static let Deposit = 500
+    var money = 0
+    required init(charge: Int){
+        money = charge - ICCard.Deposit
     }
 }
-print("end")
-        
+
+class Icocai: ICCard{
+    static var serial = 0
+    let idnumber: Int
+    
+    init(id: Int, money: Int){
+        idnumber = id
+        super.init(charge: money)
+    }
+    
+    required init(charge:Int){
+        idnumber = ++Icocai.serial
+        super.init(charge: charge)
+    }
+}
+
+let a = Icocai(charge: 1000)
+print(a.money)
