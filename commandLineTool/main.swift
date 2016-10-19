5f1e2b69f3fc7ca8836c40e4e648420c94030ccd
Yuto Mizutani <i.yuto.curiosity@gmail.com>
2016-10-19 09:57:03 +0900

    citizenを使ってプロトコルの継承までを確認

--- commandLineTool/main.swift
+++ commandLineTool/main.swift
@@ -1,25 +1,59 @@
-class Item{
-    var price: Double = 0
-    
-    //税込み価格
-    var intaxPrice: Double{
-        //値を取得するときに呼ばれる.
-        get{
-            return self.price*1.08
-        }
-        //値がセットされるときに呼ばれる
-        set(p){
-            price = p/1.08
-        }
+protocol Personal {
+    var name: String {get}
+    init(name: String)
+    func sayHelloTo(p:Personal)
+    func saySomething() -> String //プロトコル側のメソッドの宣言にはメソッドメイト引数列,返り値の型のみ.ブロックは記述しない
+}
+enum Sex { case Male, Female}
+
+protocol HealthInfo: Personal{
+    var weight: Double { get set }
+    var height: Double { get set }
+    var sex: Sex? { get }
+}
+
+class Citizen: Personal {
+    let name: String
+    required init(name:String) {
+        self.name = name
+    }
+    func sayHelloTo(p:Personal){
+        print("どうも." + p.name + "さん")
+    }
+    func saySomething() -> String{
+        return "なるほど"
     }
 }
 
-var item = Item()
-item.intaxPrice = 108
-print("税抜価格",Int(item.price),"円")
-print("税込価格",Int(item.intaxPrice),"円")
+struct Person: HealthInfo {
+    let name: String
+    let sex: Sex?
+    var weight = 0.0
+    var height = 0.0
+    init(name: String) {
+        self.name = name
+        sex = nil
+    }
+    init(name: String, sex:Sex){ // 新しいイニシャライザ
+        self.name = name
+        self.sex = sex
+    }
+    func sayHelloTo(p: Personal) {
+        if let w = p as? HealthInfo where self.sex == w.sex{
+            print("やあ," + p.name + ". ")
+        } else {
+            print("こんにちは.")
+        }
+    }
+    func saySomething() -> String {
+        return "すごいな"
+    }
+}
 
-var item2 = Item()
-item2.price = 200
-print("税抜価格",Int(item2.price),"円")
-print("税込価格",Int(item2.intaxPrice),"円")
+let sachi = Person(name: "幸", sex: Sex.Female)
+let yumi = Person(name: "由美子", sex: Sex.Female)
+let chiz = Citizen(name: "千鶴")
+sachi.sayHelloTo(yumi)
+sachi.sayHelloTo(chiz)
+
+
