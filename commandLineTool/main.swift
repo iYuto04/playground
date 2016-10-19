a1bcf7ac4969c8aacc40aa4f0dfded21e5b9e456
Yuto Mizutani <i.yuto.curiosity@gmail.com>
2016-10-19 11:33:25 +0900

    WeekDayを範囲型にした

--- commandLineTool/main.swift
+++ commandLineTool/main.swift
@@ -1,7 +1,16 @@
-enum WeekDay: Int, Comparable {
+enum WeekDay: Int, ForwardIndexType {
     case Sun, Mon, Tue, Wed, Thu, Fri, Sat
+    case _dummy
+    
+    func successor() -> WeekDay {
+        if self == WeekDay._dummy{
+            return ._dummy
+        }
+        return WeekDay(rawValue: self.rawValue + 1)!
+    }
 }
 
-func < (lhs:WeekDay, rhs:WeekDay) -> Bool{
-    return lhs.rawValue < rhs.rawValue
-} //ComparableはEquilableを継承していて「<」のみを定義すればよい
+for i in WeekDay.Mon ... .Sat {
+    print(i.rawValue, terminator: " ")
+}
+print("")
