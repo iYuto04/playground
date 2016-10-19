195e4906ac075ee546673924dd9ca07155b65cb9
Yuto Mizutani <i.yuto.curiosity@gmail.com>
2016-10-19 12:00:23 +0900

    プロトコルの継承を実装

--- commandLineTool/main.swift
+++ commandLineTool/main.swift
@@ -1,16 +1,52 @@
-enum WeekDay: Int, ForwardIndexType {
-    case Sun, Mon, Tue, Wed, Thu, Fri, Sat
-    case _dummy
+protocol Copyable {
+    func copy() -> Self
+}
+
+class GeoPoint: Copyable, Equatable, CustomStringConvertible{
+    let latitude: Double //緯度
+    let longitude: Double //経度
+    required init(lat:Double, lon:Double){ //必須イニシャライザ
+        self.latitude = lat
+        self.longitude = lon
+    }
+    func copy() -> Self { //ここでSelfとdynamicTypeを使う
     
-    func successor() -> WeekDay {
-        if self == WeekDay._dummy{
-            return ._dummy
-        }
-        return WeekDay(rawValue: self.rawValue + 1)!
+        let nw = self.dynamicType.init(lat:latitude, lon:longitude)
+        return nw
+    }
+    
+    var description: String{
+        return "GeoPoint: \(latitude), \(longitude)"
     }
 }
 
-for i in WeekDay.Mon ... .Sat {
-    print(i.rawValue, terminator: " ")
+func == (lhs: GeoPoint, rhs:GeoPoint) -> Bool{
+    return lhs.latitude == rhs.latitude && lhs.longitude == rhs.longitude
 }
-print("")
+
+class ViewPoint :GeoPoint {
+    let name: String
+    required init(lat: Double, lon: Double) {
+        self.name = "(none)"
+        super.init(lat: lat, lon: lon)
+    }
+    required init(name: String, lat: Double, lon: Double){
+        self.name = name
+        super.init(lat: lat, lon: lon)
+    }
+    override func copy() -> Self {
+        return self.dynamicType.init(name:name, lat:latitude, lon:longitude)
+    }
+    override var description : String {
+        return "ViewPoint: \(name), \(latitude), \(longitude)"
+    }
+}
+
+var g0 = GeoPoint(lat: 35.70040, lon: 139.77217)
+var g1 = g0.copy()
+print(g1)
+
+var g2 = ViewPoint(name: "松本城", lat: 36.23, lon: 137.96)
+var g3 = g2.copy()
+print(g3)
+print(g1 == g3)
